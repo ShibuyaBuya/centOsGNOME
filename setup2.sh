1c26c39b6409e4da8d41bb3d0147fad9d5c91458
ShibuyaBuya <121205226+ShibuyaBuya@users.noreply.github.com>
2023-08-05 16:14:09 +0700

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -3,4 +3,4 @@ sudo systemctl daemon-reload
 sudo systemctl enable vncserver@:4.service
 sudo firewall-cmd --permanent --zone=public --add-port=5904/tcp 
 sudo firewall-cmd --reload
-vncserver
+vncserver :4
