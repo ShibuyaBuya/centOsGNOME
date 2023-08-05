43693fa8166db8238fe0be2e79ba9fd7b94b57ee
ShibuyaBuya <121205226+ShibuyaBuya@users.noreply.github.com>
2023-08-05 16:03:30 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 sudo yum groupinstall -y "GNOME Desktop"
 reboot
 sudo yum install -y tigervnc-server 
 sudo cp /lib/systemd/system/vncserver@.service /etc/systemd/system/vncserver@:4.service 
-
+echo "Open the copied file to change the configuration"
