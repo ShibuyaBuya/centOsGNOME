6879ed3f84510d3c0d36e5a49d43d8c94cf6efaf
ShibuyaBuya <121205226+ShibuyaBuya@users.noreply.github.com>
2023-08-05 16:48:54 +0700

    Update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -5,10 +5,12 @@ sudo systemctl enable vncserver@:4.service
 sudo firewall-cmd --permanent --zone=public --add-port=5904/tcp 
 sudo firewall-cmd --reload
 vncserver :4
+echo "installing chrome..."
 yum install liberation-fonts
 yum install vulkan
 yum install wget
-
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+sudo rpm -i google-chrome-stable_current_x86_64.rpm
 sudo systemctl daemon-reload
 sudo systemctl restart vncserver@:4.service 
 
