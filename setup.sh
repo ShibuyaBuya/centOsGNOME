c0aabfb5723ac663378a4533f3a04f4b960b6e08
root <root@node37325-tuitekroid.user.cloudjkt01.com>
2023-08-06 09:29:08 +0000

    Changes setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo yum install -y tigervnc-server 
 sudo cp /lib/systemd/system/vncserver@.service /etc/systemd/system/vncserver@:4.service 
+echo "copying /lib/systemd/system/vncserver@.service -> /etc/systemd/system/vncserver@:4.service"
 echo "Open the copied file to change the configuration"
 echo "Change [Service] Like this"
 echo "[Service]"
